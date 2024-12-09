947f8af91ed197e862b7ac0c480895721fa6440d
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-12-09 15:47:35 +0200

    font family and weight done

---
+++ lib/style/fonts/font_family_helper.dart
@@ -0,0 +1,12 @@
+class FontFamilyHelper {
+  const FontFamilyHelper._();
+
+  // static final FontFamilyHelper _instance = FontFamilyHelper._();
+  // static FontFamilyHelper get instance => _instance;
+
+  static  const String cairoArabic = 'Cairo';
+  static  const String poppinsEnglish = 'Poppins';
+  //TODO: switch Arabic font and English Font
+
+  
+}
