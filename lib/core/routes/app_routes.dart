c70277a625f071e0d84d25ff28f7f44236ab16f4
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-12-26 16:37:58 +0200

    sign up ui done

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:asroo_store/core/common/screen/under_build_screen.dart';
 import 'package:asroo_store/core/features/auth/presentation/refactors/login_body.dart';
 import 'package:asroo_store/core/features/auth/presentation/screens/login_screen.dart';
+import 'package:asroo_store/core/features/auth/presentation/screens/sign_up_screen.dart';
 import 'package:asroo_store/core/routes/base_routes.dart';
 
 import 'package:flutter/material.dart';
@@ -9,7 +10,6 @@ import 'package:flutter/material.dart';
 
 class AppRoutes {
   static const String login = 'login';
-  static const String loginBody = 'loginBody';
   static const String signUp = 'signUp';
 
   static Route<void> onGenerateRoute(RouteSettings settings) {
@@ -18,8 +18,8 @@ class AppRoutes {
     switch (settings.name) {
       case login:
         return BaseRoute(page: const LoginScreen());
-      case loginBody:
-        return BaseRoute(page: const LoginBody());
+      case signUp:
+        return BaseRoute(page: const SignUpScreen());
       default:
         return BaseRoute(page:const PageUnderBuildScreen());
     }
