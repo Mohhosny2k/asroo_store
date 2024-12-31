2441e1edc6e5306e085f49eb76d1db8036bd7242
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-12-31 17:05:43 +0200

    dio factory

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,18 +1,21 @@
 import 'package:asroo_store/core/app/app_cubit/app_cubit.dart';
+import 'package:asroo_store/core/service/graphql/api_service.dart';
+import 'package:asroo_store/core/service/graphql/dio_factory.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 
-
 Future<void> setupInjector() async {
 //  sl.registerLazySingleton(() => ConnectivityController.instance);
   //sl.registerLazySingleton(() => SharedPref());
-await  _initCore();
+  await _initCore();
   // sl.registerLazySingleton(() => AppBlocObserver());
   // sl.registerLazySingleton(() => EnvVariable());
   // sl.registerLazySingleton(() => Firebase.initializeApp());
 }
 
-Future<void> _initCore() async{
-   sl.registerLazySingleton(AppCubit.new);
+Future<void> _initCore() async {
+  final dio = DioFactory.getDio();
+  sl.registerLazySingleton(AppCubit.new);
+  sl.registerLazySingleton<ApiService>(() => ApiService(dio));
 }
--- lib/core/service/shared_pref/pref_keys.dart
+++ lib/core/service/shared_pref/pref_keys.dart
@@ -1,5 +1,6 @@
 class PrefKeys {
   static const String themeMode = 'mode';
   static const String language = 'language';
+  static const String accessToken = 'accessToken';
   
 }
