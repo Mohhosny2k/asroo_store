5f3b2b206384f1c7e6c41decd87e863d21dc8a12
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2025-01-29 15:55:15 +0200

    Add sign-up  bloc pattern

--- lib/core/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/core/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,49 +1,79 @@
 import 'dart:async';
-import '../../data/models/login_request_body.dart';
-import '../../data/repos/auth_repo.dart';
-import '../../../../service/shared_pref/pref_keys.dart';
-import '../../../../service/shared_pref/shared_pref.dart';
+import 'package:asroo_store/core/features/auth/data/models/login_request_body.dart';
+import 'package:asroo_store/core/features/auth/data/models/sign_up_request_body.dart';
+import 'package:asroo_store/core/features/auth/data/repos/auth_repo.dart';
+import 'package:asroo_store/core/service/shared_pref/pref_keys.dart';
+import 'package:asroo_store/core/service/shared_pref/shared_pref.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import "package:flutter_bloc/flutter_bloc.dart";
+
 part 'auth_event.dart';
 part 'auth_state.dart';
 part 'auth_bloc.freezed.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc(this._repos) : super(const _Initial()) {
+  AuthBloc(this._repo) : super(const _Initial()) {
     on<LoginEvent>(_login);
+    on<SignUpEvent>(_signUp);
   }
-  final AuthRepos _repos;
+
+  final AuthRepos _repo;
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
 
   final formKey = GlobalKey<FormState>();
 
-  //? login
+//Login
   FutureOr<void> _login(LoginEvent event, Emitter<AuthState> emit) async {
     emit(const AuthState.loading());
 
-    final result = await _repos.login(
-        body: LoginRequestBody(
-            email: emailController.text.trim(),
-            password: passwordController.text));
+    final result = await _repo.login(
+      LoginRequestBody(
+        email: emailController.text.trim(),
+        password: passwordController.text,
+      ),
+    );
 
     await result.when(
-      success: (response) async {
-        //* user Token
-        final token = response.data.login.accessToken ?? '';
-
-        //* save token in shared pref
+      success: (loginData) async {
+        // user token
+        final token = loginData.data.login.accessToken ?? '';
+        // save token in shared preferences
         await SharedPref().setString(PrefKeys.accessToken, token);
+        // get user role
+        final user = await _repo.userRole(token);
+        await SharedPref().setInt(PrefKeys.userId, user.userId ?? 0);
+        await SharedPref().setString(PrefKeys.userRole, user.userRole ?? '');
+        //await _repo.addUserIdFirebase(userId: user.userId.toString());
+        emit(AuthState.success(userRole: user.userRole ?? ''));
+      },
+      failure: (error) {
+        emit(AuthState.error(error: error));
+      },
+    );
+  }
 
-        //* get user role
+  // signup and login to take user token
+  FutureOr<void> _signUp(
+    SignUpEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(const AuthState.loading());
+    final result = await _repo.signUp(
+      SignUpRequestBody(
+        email: emailController.text.trim(),
+        password: passwordController.text,
+        avatar: event.imagUrl,
+        name: nameController.text.trim(),
+      ),
+    );
 
-        final user = await _repos.userRole(token);
-        await SharedPref().setInt(PrefKeys.userId, user.userId ??0);
-        await SharedPref().setString(PrefKeys.userRole, user.userRole ??'');
-        emit( AuthState.success(userRole: user.userRole ?? ''));
+    result.when(
+      success: (signupData) {
+        add(const AuthEvent.login());
       },
       failure: (error) {
         emit(AuthState.error(error: error));
