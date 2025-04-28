067e8507815babc338f57b953fe58aebdaee1df5
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2025-04-28 17:08:47 +0300

    package firebase messa

--- lib/features/customer/home_customer.dart
+++ lib/features/customer/home_customer.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:asroo_store/core/extensions/context_extension.dart';
 
@@ -12,8 +13,12 @@ class HomeCustomerScreen extends StatelessWidget {
          backgroundColor: context.color.mainColor,
         title: const Text('Home Customer' , style: TextStyle(color: Colors.white),),
       ),
-      body: const Center(
-        child: Text('Home Customer Screen'),
+      body:  Center(
+        child: InkWell(
+          onTap: () async{
+            final token =await FirebaseMessaging.instance.getToken();
+          },
+          child: Text('Home Customer Screen')),
       ),
     );
   }
