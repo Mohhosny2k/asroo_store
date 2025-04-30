8ec88568fc221d0d1220645752f3022de1e80e06
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2025-04-30 15:36:00 +0300

    Refactor sendNotification method to use topic subscription for notifications

--- lib/core/service/push_notification/firebase_cloud_messaging.dart
+++ lib/core/service/push_notification/firebase_cloud_messaging.dart
@@ -9,43 +9,41 @@ class FirebaseCloudMessaging {
   FirebaseCloudMessaging._();
 
   static final FirebaseCloudMessaging _instance = FirebaseCloudMessaging._();
+
+  static const String subscribeKey = 'asroo_store';
 // send notification to device with api
   Future<void> sendNotification() async {
- try{
-     final response = await Dio().post<dynamic>(
-        EnvVariable.instance.notificationBaseUrl,
-        options: Options(
-            contentType: Headers.jsonContentType,
-            responseType: ResponseType.json,
-            validateStatus: (_) => true,
-            headers: {
-              'Authorization':
-                  'key=${EnvVariable.instance.firebaseKey}',
-              'Content-Type': 'application/json',
-            }
-            
-
-            
-            ) , data: {
-              'to': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjcyLCJpYXQiOjE3MzgyMjk4NzAsImV4cCI6MTczOTk1Nzg3MH0.EXOUSBqWiK_myjrJ1vV6WWMaMGGzUuJo4KTMKKLTG9M',
-              'notification': {
-                'title': 'Check this Mobile {title}',
-                'body': 'Rich Notification {body}',
-                //'android_channel_id': 'high_importance_channel',
-                //'priority': 'high',
-                // 'mutable_content': true,
-                // 'sound': 'Tri_tone',
-              },
-              // 'data': {
-              //   'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-              //   'id': '1',
-              //   'status': 'done'
-              // }
-            });
-            debugPrint
-('Response Notification Created: ${response.data}');
-  } catch (e) {
-    print('Error sending notification: $e');
+    try {
+      final response = await Dio().post<dynamic>(
+          EnvVariable.instance.notificationBaseUrl,
+          options: Options(
+              contentType: Headers.jsonContentType,
+              responseType: ResponseType.json,
+              validateStatus: (_) => true,
+              headers: {
+                'Authorization': 'key=${EnvVariable.instance.firebaseKey}',
+                'Content-Type': 'application/json',
+              }),
+          data: {
+            'to': '/topics/$subscribeKey',
+            //'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjcyLCJpYXQiOjE3MzgyMjk4NzAsImV4cCI6MTczOTk1Nzg3MH0.EXOUSBqWiK_myjrJ1vV6WWMaMGGzUuJo4KTMKKLTG9M',
+            'notification': {
+              'title': 'Check this Mobile {title}',
+              'body': 'Rich Notification {body}',
+              //'android_channel_id': 'high_importance_channel',
+              //'priority': 'high',
+              // 'mutable_content': true,
+              // 'sound': 'Tri_tone',
+            },
+            // 'data': {
+            //   'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            //   'id': '1',
+            //   'status': 'done'
+            // }
+          });
+      debugPrint('Response Notification Created: ${response.data}');
+    } catch (e) {
+      print('Error sending notification: $e');
+    }
   }
- }
 }
